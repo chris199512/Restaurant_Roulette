fec4a34bdb0b9fba8a10b350d4a938a2fdcac397
chris199512 <chris199@live.de>
2023-03-11 13:52:59 +0100

    Roulette hinzugefügt und Rotation eingebaut

--- app/src/main/java/com/smarthomebear/restaurantroulette/MainActivity.java
+++ app/src/main/java/com/smarthomebear/restaurantroulette/MainActivity.java
@@ -3,12 +3,25 @@ package com.smarthomebear.restaurantroulette;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView2);
     }
+
+    public void start(View view) {
+        Animation rotateImage= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
+        imageView.startAnimation(rotateImage);
+    }
+
 }
