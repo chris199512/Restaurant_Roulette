0fcc49a4bb0f735f98144573e2033a77f8013530
Kubar Yarcu <kuebar-yarcu@hotmail.de>
2023-04-02 20:25:46 +0200

    Popfenster wird jetzt beim Anlicken vom Filter-Button angezeigt

--- app/src/main/java/com/smarthomebear/restaurantroulette/RouletteFragment.java
+++ app/src/main/java/com/smarthomebear/restaurantroulette/RouletteFragment.java
@@ -1,6 +1,6 @@
 package com.smarthomebear.restaurantroulette;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,42 +9,78 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class RouletteFragment extends Fragment implements View.OnClickListener{
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouletteFragment extends Fragment implements View.OnClickListener {
 
     ImageView imageViewRoulette;
     Button filterButton;
     Animation rotateImage;
 
+    private List<String> selectedItems = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_roulette, container, false);
-        Button filter=(Button) view.findViewById(R.id.filter);
+        Button filter = (Button) view.findViewById(R.id.filter);
 
         filter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction fr= getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container, new PopFragment());
-                fr.commit();
+                showPreferencesDialog();
             }
         });
+
         return view;
-
-
     }
-
 
     @Override
     public void onClick(View view) {
 
     }
+
+    private void showPreferencesDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Ernährungspräferenzen auswählen")
+                .setMultiChoiceItems(R.array.preferences, null,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                String[] preferences = getResources().getStringArray(R.array.preferences);
+                                String selectedItem = preferences[which];
+                                if (isChecked) {
+                                    selectedItems.add(selectedItem);
+                                } else {
+                                    selectedItems.remove(selectedItem);
+                                }
+                            }
+                        })
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Do something with the selected items
+                        Toast.makeText(getActivity(), "Selected items: " + selectedItems.toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // do nothing
+                    }
+                });
+        builder.create().show();
+    }
 }
+
 
