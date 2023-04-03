07ce158ff4816434d9d6620d95cdbec65bd459f1
chris199512 <chris199@live.de>
2023-04-03 19:14:23 +0200

    Datenschutztaste funktionsfähig gemacht

--- app/src/main/java/com/smarthomebear/restaurantroulette/MainActivity.java
+++ app/src/main/java/com/smarthomebear/restaurantroulette/MainActivity.java
@@ -1,13 +1,16 @@
 package com.smarthomebear.restaurantroulette;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -84,6 +87,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             case R.id.nav_questions:
                 Toast.makeText(this, "Q&A", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_dsgvo:
+                AlertDialog.Builder alert=new AlertDialog.Builder(this);
+                alert.setTitle("Datenschutzerklärung");
+                alert.setMessage("Hier steht Text");
+                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                alert.create().show();
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
